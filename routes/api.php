e8607e020dda9d7b2fabe6fd09418ccb5662ec54
Usenmfon <usenmfonuko@gmail.com>
2023-09-25 22:52:11 +0100

    updated code

--- routes/api.php
+++ routes/api.php
@@ -23,7 +23,7 @@ use Illuminate\Support\Facades\Route;
 Route::group([
     'middleware' => 'api',
     'prefix' => 'auth'
-], function($router) {
+], function() {
     Route::post('/login', [AuthController::class, 'login']);
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/logout', [AuthController::class, 'logout']);
